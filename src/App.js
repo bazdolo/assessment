5d6890e1efcabb5026a973b096a3702c7198a998
Barry Dolo <Barry@getgokart.com>
2020-10-31 20:18:26 +0100

    extended the numbersToWordsHelper function to accept the range 20 - 99 and added error checking also updated tests to reflect new range of values and edited boudaries

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 
 const units = [
@@ -23,26 +24,40 @@ const units = [
 	'nineteen'
 ];
 
+const tens = [ null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+
 export const numbersToWordsHelper = (num) => {
-	if (num < 20 && num >= 0) {
+	if (num < 0) return 'Number not within range';
+	if (num < 20) {
 		return units[num];
+	}
+	if (num < 100) {
+		const remainder = num % 10;
+		if (!remainder) {
+			return tens[Math.floor(num / 10)];
+		}
+		return tens[Math.floor(num / 10)] + '-' + numbersToWordsHelper(remainder);
 	} else return 'Number not within range';
 };
 
 function App() {
+	const [ num2Word, setNum2Word ] = useState('');
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const parsedString = parseInt(e.target[0].value);
-		console.log(numbersToWordsHelper(parsedString));
+		const result = numbersToWordsHelper(parsedString);
+		setNum2Word(result);
 	};
 
 	return (
 		<div className="App">
 			<form onSubmit={handleSubmit}>
-				<label>Enter any number from 0 to below a million</label>
+				<label>Enter any number from zero to below a million</label>
 				<input className="number-input" type="number" name="name" />
 				<input className="submit-button" type="submit" value="Submit" />
 			</form>
+			<div>{num2Word}</div>
 		</div>
 	);
 }
--- src/App.test.js
+++ src/App.test.js
@@ -7,12 +7,21 @@ test('renders form without crashing', () => {
 	ReactDOM.render(<App />, div);
 });
 
+// test data type
+test('return type is equal to string', () => {
+	const result = numbersToWordsHelper(17);
+	expect(typeof result).toBe('string');
+});
+
+////// Test cases for values below 20
+
 // Normal mid point value under 20
 test('10 to equal ten', () => {
 	const result = numbersToWordsHelper(10);
 	expect(result).toBe('ten');
 });
 
+// not equal to value
 test('10 to NOT equal eleven', () => {
 	const result = numbersToWordsHelper(10);
 	expect(result).not.toBe('eleven');
@@ -37,7 +46,39 @@ test('-1 to equal Number not within range', () => {
 });
 
 // Extends Boundary
-test('20 to equal Number not within range', () => {
+// test('20 to equal Number not within range', () => {
+// 	const result = numbersToWordsHelper(20);
+// 	expect(result).toBe('Number not within range');
+// });
+
+////// Test cases for between 20 - 100
+
+// Normal mid point value without -
+test('50 to equal fifty', () => {
+	const result = numbersToWordsHelper(50);
+	expect(result).toBe('fifty');
+});
+
+// Normal mid point value with -
+test('55 to equal fifty-five', () => {
+	const result = numbersToWordsHelper(55);
+	expect(result).toBe('fifty-five');
+});
+
+// Lower Boundary
+test('20 to equal twenty', () => {
 	const result = numbersToWordsHelper(20);
+	expect(result).toBe('twenty');
+});
+
+// Upper Boundary under 100
+test('99 to equal ninety-nine', () => {
+	const result = numbersToWordsHelper(99);
+	expect(result).toBe('ninety-nine');
+});
+
+// Extends Boundary
+test('100 to equal Number not within range', () => {
+	const result = numbersToWordsHelper(100);
 	expect(result).toBe('Number not within range');
 });
